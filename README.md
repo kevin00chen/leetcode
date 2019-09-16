38b2d6eacb0ff0e50f5a673e194525c87177e22b
kaimingchen <kaimingchen@xiaohongshu.com>
2019-09-16 14:29:21 +0800

    Solution1

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# leetcode
+　　题目列表
+
+### 1 两数求和
+问题描述：
+
+　　给定一个整数数组，返回和为指定数字的元素下标。每个元素只能使用一次。
+
+示例：
+
+nums = [2, 7, 11, 15], target = 9,
+
+nums[0] + nums[1] = 2 + 7 = 9,
+
+返回 [0, 1].
+
+解法：
+
+　　除了两层遍历的暴力解法之外，还可以用HashMap来解决，遍历数组，target - 当前元素 之差如果不在该map中，则map中添加元素，否则直接返回下标。
+
+[Solution](./src/main/java/com/ckm/normal/Solution1.java)
---
+++ src/test/java/com/ckm/normal/SolutionTest.java
@@ -0,0 +1,15 @@
+package com.ckm.normal;
+
+
+public class SolutionTest {
+
+    public static void main(String[] args) {
+        testSolution1();
+    }
+
+    private static void testSolution1() {
+        Solution1 solution = new Solution1();
+        int[] s = solution.twoSum(new int[]{2, 7, 11, 15}, 9);
+        System.out.println();
+    }
+}
